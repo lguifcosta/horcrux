fd9672bb5a51e0563d91aaeaf07110e6c1fb0fae
lguifcosta <lgcosta.lgfc@gmail.com>
2020-12-30 20:29:40 -0300

    final mods

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const server = express()
 
 
-const { pageLanding, donate, register, saveOng , pageMore , login , loged , manager , admit } = require('./pages')
+const { pageLanding, donate, register, saveOng , pageMore , login , loged , manager , admit } = require('./src/pages')
 //configurar o nunjucks(template engine)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {

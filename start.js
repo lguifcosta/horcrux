81c8f923ec0717a6e9dc054b8c223882a811acae
lguifcosta <lgcosta.lgfc@gmail.com>
2021-01-06 16:12:13 -0300

    final mods

--- start.js
+++ start.js
@@ -4,24 +4,24 @@ const express = require('express')
 const server = express()
 
 
-const { pageLanding, donate, register, saveOng , pageMore , login , loged , manager , admit } = require('./src/pages')
+const { pageLanding, donate, register, saveOng, pageMore, login, loged, manager, admit } = require('./src/pages')
 //configurar o nunjucks(template engine)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
-    express:server,
+    express: server,
     noCache: true,
 })
 //inicio e configuração do servidor
 //receber os dados do rec.body
-server.use(express.urlencoded({ extended: false}))
+server.use(express.urlencoded({ extended: false }))
 //configurar arquivos estaticos (css, scripts, imagens)
 server.use(express.static("public"))
 //rotas de aplicação
 server.get("/", pageLanding)
 server.get("/ajudar", donate)
 server.get("/cadastro", register)
-server.post("/saveOng" , saveOng)    
-server.get("/more", pageMore )
+server.post("/saveOng", saveOng)
+server.get("/more", pageMore)
 server.get("/log", login)
 server.post("/loged", loged)
 server.get("/654484854814815121494611vdvvds", manager)
@@ -29,5 +29,5 @@ server.post("/admit", admit);
 //start do servidor
 const port = process.env.PORT || 3000;
 
-server.listen(port);
+//server.listen(port);
 console.log(`a porta é ${port}`)
